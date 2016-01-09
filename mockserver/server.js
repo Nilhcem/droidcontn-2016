5137746b9162c036f1a3f2407b1305261c8fc98b
Gautier Nilhcem <nilhcem@gmail.com>
2016-01-09 12:31:40 +0100

    Add mock server

---
+++ mockserver/server.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 8990);
+app.use(express.static(__dirname + '/public'));
+
+var port = app.get('port');
+var defaultCharset = 'utf8';
+
+/* Replaces 'localhost:8080' url with actual server url */
+function replaceUrl(data, req) {
+    var host = req.protocol + '://' + req.hostname + (port == 80 || port == 443 ? '' : ':' + port);
+    return data.replace(/http:\/\/localhost:8080/g, host);
+}
+
+/* Agenda */
+app.get('/model/agenda.json', function(req, res) {
+    res.type('application/json; charset=' + defaultCharset);
+    res.status(200).send(fs.readFileSync('data/agenda.json', defaultCharset));
+});
+
+/* Speakers */
+app.get('/model/speakers.json', function(req, res) {
+    res.type('application/json; charset=' + defaultCharset);
+    res.status(200).send(fs.readFileSync('data/speakers.json', defaultCharset));
+});
+
+/* Default (home page) */
+app.get('^*$', function(req, res) {
+    var data = fs.readFileSync('data/index.html', defaultCharset);
+    data = replaceUrl(data, req);
+
+    res.type('text/html; charset=' + defaultCharset);
+    res.status(200).send(data);
+});
+
+/* Starts server */
+app.listen(port, function () {
+    console.log('Express server listening on port ' + port);
+});
