fd9ff0b63b7cec10410c0253ff14537663422688
Gautier Nilhcem <nilhcem@gmail.com>
2016-01-09 14:54:40 +0100

    Remove unused stuff

--- app/src/main/java/com/nilhcem/droidcontn/ui/drawer/DrawerActivity.java
+++ app/src/main/java/com/nilhcem/droidcontn/ui/drawer/DrawerActivity.java
@@ -1,9 +1,7 @@
 package com.nilhcem.droidcontn.ui.drawer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.nilhcem.droidcontn.R;
 
@@ -24,15 +21,6 @@ public class DrawerActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
