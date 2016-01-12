ee034f6d832b076b1f3ff25238845c3c16ca4c20
Gautier Nilhcem <nilhcem@gmail.com>
2016-01-12 21:13:12 +0100

    Add takt to measure fps

--- app/src/internal/java/com/nilhcem/droidcontn/InternalDroidconApp.java
+++ app/src/internal/java/com/nilhcem/droidcontn/InternalDroidconApp.java
@@ -1,11 +1,21 @@
 package com.nilhcem.droidcontn;
 
+import android.os.Build;
+
 import com.nilhcem.droidcontn.core.dagger.AppComponent;
 import com.nilhcem.droidcontn.debug.stetho.StethoInitializer;
 
 import javax.inject.Inject;
 
+import jp.wasabeef.takt.Takt;
+
 public class InternalDroidconApp extends DroidconApp {
+
+    /**
+     * Change it manually when you want to display the FPS.
+     * Useful to test the frame rate.
+     */
+    private static final boolean DISPLAY_FPS = false;
 
     @Inject StethoInitializer mStetho;
 
@@ -13,6 +23,24 @@ public class InternalDroidconApp extends DroidconApp {
     public void onCreate() {
         super.onCreate();
         AppComponent.Initializer.init(this).inject(this);
+        displayFps(true);
         mStetho.init();
+    }
+
+
+    @Override
+    public void onTerminate() {
+        displayFps(false);
+        super.onTerminate();
+    }
+
+    private void displayFps(boolean enable) {
+        if (DISPLAY_FPS && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            if (enable) {
+                Takt.stock(this).play();
+            } else {
+                Takt.finish();
+            }
+        }
     }
 }
