eb2cd17ba0245d7432eb04f6d0676830cbc587df
Gautier Nilhcem <nilhcem@gmail.com>
2016-01-23 19:51:53 +0100

    Start creating schedule day entry layout

--- app/src/main/java/com/nilhcem/droidcontn/ui/schedule/day/ScheduleDayEntry.java
+++ app/src/main/java/com/nilhcem/droidcontn/ui/schedule/day/ScheduleDayEntry.java
@@ -1,23 +1,57 @@
 package com.nilhcem.droidcontn.ui.schedule.day;
 
+import android.support.v4.content.ContextCompat;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.nilhcem.droidcontn.R;
+import com.nilhcem.droidcontn.data.model.Session;
 import com.nilhcem.droidcontn.data.model.Slot;
 import com.nilhcem.droidcontn.ui.core.recyclerview.BaseViewHolder;
+import com.nilhcem.droidcontn.utils.Views;
+
+import java.util.List;
 
 import butterknife.Bind;
 
 public class ScheduleDayEntry extends BaseViewHolder {
 
-    @Bind(R.id.schedule_day_entry_text) TextView name;
+    @Bind(R.id.schedule_day_entry_time) TextView time;
+    @Bind(R.id.schedule_day_entry_desc) TextView desc;
 
     public ScheduleDayEntry(ViewGroup parent) {
         super(parent, R.layout.schedule_day_entry);
     }
 
     public void bindSlot(Slot slot) {
-        name.setText(slot.getFromTime() + " - " + slot.getToTime());
+        List<Session> sessions = slot.getSessions();
+        if (sessions.size() == 1) {
+            Session session = sessions.get(0);
+            if (session.getSpeakersId() == null) {
+                bindBreakSlot(session);
+            } else {
+                bindChosenSlot(session);
+            }
+        } else {
+            bindFreeSlot();
+        }
+        time.setText(slot.getFromTime());
+    }
+
+    private void bindFreeSlot() {
+        Views.setBackground(desc, R.drawable.schedule_day_entry_free);
+        desc.setText(R.string.schedule_browse_sessions);
+        desc.setTextColor(ContextCompat.getColor(desc.getContext(), R.color.primary));
+    }
+
+    private void bindBreakSlot(Session session) {
+        Views.setBackground(desc, R.drawable.schedule_day_entry_break);
+        desc.setText(session.getTitle());
+        desc.setTextColor(ContextCompat.getColor(desc.getContext(), R.color.primary_text));
+    }
+
+    private void bindChosenSlot(Session session) {
+        // TODO
+        bindBreakSlot(session);
     }
 }
--- app/src/main/java/com/nilhcem/droidcontn/utils/Views.java
+++ app/src/main/java/com/nilhcem/droidcontn/utils/Views.java
@@ -1,6 +1,10 @@
 package com.nilhcem.droidcontn.utils;
 
 import android.annotation.TargetApi;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
 import android.view.ViewTreeObserver;
 
 import static android.os.Build.VERSION_CODES.JELLY_BEAN;
@@ -9,6 +13,22 @@ public final class Views {
 
     private Views() {
         throw new UnsupportedOperationException();
+    }
+
+    @TargetApi(JELLY_BEAN)
+    @SuppressWarnings("deprecation")
+    public static void setBackground(View view, @DrawableRes int drawableRes) {
+        setBackground(view, ContextCompat.getDrawable(view.getContext(), drawableRes));
+    }
+
+    @TargetApi(JELLY_BEAN)
+    @SuppressWarnings("deprecation")
+    public static void setBackground(View view, Drawable drawable) {
+        if (AppUtils.isCompatible(JELLY_BEAN)) {
+            view.setBackground(drawable);
+        } else {
+            view.setBackgroundDrawable(drawable);
+        }
     }
 
     @TargetApi(JELLY_BEAN)
