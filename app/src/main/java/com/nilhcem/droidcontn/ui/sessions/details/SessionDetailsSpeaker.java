ca9885ec25cafee3a5bebfbb5dacc5a9dbf39983
Gautier Nilhcem <nilhcem@gmail.com>
2016-02-14 19:30:59 +0100

    Add selector

--- app/src/main/java/com/nilhcem/droidcontn/ui/sessions/details/SessionDetailsSpeaker.java
+++ app/src/main/java/com/nilhcem/droidcontn/ui/sessions/details/SessionDetailsSpeaker.java
@@ -2,23 +2,21 @@ package com.nilhcem.droidcontn.ui.sessions.details;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.os.Build;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.nilhcem.droidcontn.R;
 import com.nilhcem.droidcontn.data.app.model.Speaker;
 import com.nilhcem.droidcontn.ui.core.picasso.CircleTransformation;
-import com.nilhcem.droidcontn.utils.App;
 import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class SessionDetailsSpeaker extends LinearLayout {
+public class SessionDetailsSpeaker extends FrameLayout {
 
     @Bind(R.id.session_details_speaker_photo) ImageView photo;
     @Bind(R.id.session_details_speaker_name) TextView name;
@@ -27,16 +25,10 @@ public class SessionDetailsSpeaker extends LinearLayout {
     public SessionDetailsSpeaker(Context context, Speaker speaker, Picasso picasso) {
         super(context);
 
-        setOrientation(HORIZONTAL);
         int[] attrs = new int[]{android.R.attr.selectableItemBackground};
         TypedArray ta = context.obtainStyledAttributes(attrs);
-        if (App.isCompatible(Build.VERSION_CODES.M)) {
-            setForeground(ta.getDrawable(0));
-        }
+        setForeground(ta.getDrawable(0));
         ta.recycle();
-
-        int padding = context.getResources().getDimensionPixelSize(R.dimen.activity_margin);
-        setPadding(0, 0, 0, padding);
 
         LayoutInflater.from(context).inflate(R.layout.session_details_speaker, this);
         ButterKnife.bind(this, this);
