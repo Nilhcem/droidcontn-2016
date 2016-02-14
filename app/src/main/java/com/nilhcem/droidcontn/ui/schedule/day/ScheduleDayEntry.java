b5213394c876eb9f7f77d6303426918a327cb2bb
Gautier Nilhcem <nilhcem@gmail.com>
2016-02-14 14:33:22 +0100

    Review ScheduleDayEntry layout

--- app/src/main/java/com/nilhcem/droidcontn/ui/schedule/day/ScheduleDayEntry.java
+++ app/src/main/java/com/nilhcem/droidcontn/ui/schedule/day/ScheduleDayEntry.java
@@ -1,31 +1,26 @@
 package com.nilhcem.droidcontn.ui.schedule.day;
 
-import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
-import android.text.TextUtils;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nilhcem.droidcontn.R;
 import com.nilhcem.droidcontn.data.app.model.ScheduleSlot;
 import com.nilhcem.droidcontn.data.app.model.Session;
 import com.nilhcem.droidcontn.data.app.model.Speaker;
-import com.nilhcem.droidcontn.ui.core.picasso.CircleTransformation;
 import com.nilhcem.droidcontn.ui.core.recyclerview.BaseViewHolder;
-import com.nilhcem.droidcontn.utils.App;
-import com.nilhcem.droidcontn.utils.Views;
 import com.squareup.picasso.Picasso;
 
+import org.threeten.bp.LocalDateTime;
 import org.threeten.bp.format.DateTimeFormatter;
 import org.threeten.bp.format.FormatStyle;
+import org.threeten.bp.temporal.ChronoUnit;
 
 import java.util.List;
 
 import butterknife.Bind;
+import java8.util.stream.StreamSupport;
 
 public class ScheduleDayEntry extends BaseViewHolder {
 
@@ -36,12 +31,18 @@ public class ScheduleDayEntry extends BaseViewHolder {
     }
 
     @Bind(R.id.schedule_day_entry_time) TextView time;
-    @Bind(R.id.schedule_day_entry_slot_container) FrameLayout slotContainer;
-    @Bind(R.id.schedule_day_entry_slot_image) ImageView slotImage;
-    @Bind(R.id.schedule_day_entry_slot_name) TextView slotName;
-    @Bind(R.id.schedule_day_entry_slot_desc) TextView slotDesc;
 
-    private final Drawable selectableItemBackground;
+    @Bind(R.id.schedule_day_entry_break_card) CardView breakCard;
+    @Bind(R.id.schedule_day_entry_break_text) TextView breakText;
+
+    @Bind(R.id.schedule_day_entry_browse_card) CardView browseCard;
+
+    @Bind(R.id.schedule_day_entry_session_card) CardView sessionCard;
+    @Bind(R.id.schedule_day_entry_session_title) TextView sessionTitle;
+    @Bind(R.id.schedule_day_entry_session_time) TextView sessionTime;
+    @Bind(R.id.schedule_day_entry_session_room) TextView sessionRoom;
+    @Bind(R.id.schedule_day_entry_slot_speakers) ViewGroup sessionSpeakers;
+
     private final Picasso picasso;
     private final OnSessionClickListener listener;
 
@@ -49,84 +50,65 @@ public class ScheduleDayEntry extends BaseViewHolder {
         super(parent, R.layout.schedule_day_entry);
         this.picasso = picasso;
         this.listener = listener;
-
-        int[] attrs = new int[]{android.R.attr.selectableItemBackground};
-        TypedArray ta = itemView.getContext().obtainStyledAttributes(attrs);
-        selectableItemBackground = ta.getDrawable(0);
-        ta.recycle();
     }
 
     public void bindFreeSlot(ScheduleSlot slot) {
-        slotName.setText(R.string.schedule_browse_sessions);
-        slotName.setTextColor(ContextCompat.getColor(slotName.getContext(), R.color.primary));
-        slotDesc.setVisibility(View.GONE);
+        breakCard.setVisibility(View.GONE);
+        sessionCard.setVisibility(View.GONE);
 
-        Views.setBackground(slotContainer, R.drawable.schedule_day_entry_free);
-        slotContainer.setForeground(selectableItemBackground);
-        slotContainer.setOnClickListener(v -> listener.onFreeSlotClicked(slot));
-
-        slotImage.setVisibility(View.GONE);
         bindTime(slot);
+        browseCard.setVisibility(View.VISIBLE);
+        browseCard.setOnClickListener(v -> listener.onFreeSlotClicked(slot));
     }
 
     public void bindBreakSlot(ScheduleSlot slot, Session session) {
-        slotName.setText(session.getTitle());
-        slotName.setTextColor(ContextCompat.getColor(slotName.getContext(), R.color.primary_text));
-        slotDesc.setVisibility(View.GONE);
+        browseCard.setVisibility(View.GONE);
+        sessionCard.setVisibility(View.GONE);
 
-        Views.setBackground(slotContainer, R.drawable.schedule_day_entry_break);
-        slotContainer.setForeground(null);
-        slotContainer.setOnClickListener(null);
-
-        slotImage.setVisibility(View.GONE);
         bindTime(slot);
+        breakCard.setVisibility(View.VISIBLE);
+        breakText.setText(session.getTitle());
     }
 
     public void bindSelectedSession(ScheduleSlot slot, Session session) {
-        slotName.setText(session.getTitle());
-        slotName.setTextColor(ContextCompat.getColor(slotName.getContext(), R.color.primary_text));
-        bindDescription(session);
+        breakCard.setVisibility(View.GONE);
+        browseCard.setVisibility(View.GONE);
 
-        Views.setBackground(slotContainer, R.drawable.schedule_day_entry_free);
-        slotContainer.setForeground(selectableItemBackground);
-        slotContainer.setOnClickListener(v -> listener.onSelectedSessionClicked(session));
-
-        String url = App.getPhotoUrl(session);
-        if (!TextUtils.isEmpty(url)) {
-            picasso.load(url).transform(new CircleTransformation()).into(slotImage);
-        }
-        slotImage.setVisibility(View.VISIBLE);
         bindTime(slot);
+        sessionCard.setVisibility(View.VISIBLE);
+        sessionTitle.setText(session.getTitle());
+        sessionTime.setText(formatSessionTime(session));
+        sessionRoom.setText(session.getRoom());
+        bindSessionSpeakers(session);
+        sessionCard.setOnClickListener(v -> listener.onSelectedSessionClicked(session));
     }
 
-    private void bindDescription(Session session) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(session.getRoom()).append("\n");
-        String separator = null;
-
-        List<Speaker> speakers = session.getSpeakers();
-        if (speakers != null) {
-            boolean addSeparator = false;
-            for (Speaker speaker : speakers) {
-                if (addSeparator) {
-                    if (separator == null) {
-                        separator = itemView.getContext().getString(R.string.schedule_speakers_separator);
-                    }
-                    sb.append(separator);
-                } else {
-                    addSeparator = true;
-                }
-                sb.append(speaker.getName());
-            }
-        }
-
-        slotDesc.setText(sb.toString());
-        slotDesc.setVisibility(View.VISIBLE);
+    private String formatTime(LocalDateTime time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        return time.format(formatter);
     }
 
     private void bindTime(ScheduleSlot slot) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
-        String timeStr = slot.getTime().format(formatter);
+        String timeStr = formatTime(slot.getTime());
         time.setText(timeStr);
+    }
+
+    private String formatSessionTime(Session session) {
+        LocalDateTime fromTime = session.getFromTime();
+        LocalDateTime toTime = session.getToTime();
+        long minutes = ChronoUnit.MINUTES.between(fromTime, toTime);
+        return itemView.getContext().getString(R.string.schedule_day_entry_session_time_format,
+                formatTime(fromTime), formatTime(toTime), minutes);
+    }
+
+    private void bindSessionSpeakers(Session session) {
+        sessionSpeakers.removeAllViews();
+
+        List<Speaker> speakers = session.getSpeakers();
+        if (speakers != null) {
+            StreamSupport.stream(speakers)
+                    .map(speaker -> new ScheduleDayEntrySpeaker(sessionSpeakers.getContext(), speaker, picasso))
+                    .forEach(sessionSpeakers::addView);
+        }
     }
 }
